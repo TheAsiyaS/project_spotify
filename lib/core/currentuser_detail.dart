d32c574389b034eb10a6966619d3b69cc4de5c0c
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-10 13:29:22 +0530

    divider

--- lib/core/currentuser_detail.dart
+++
@@ -1,3 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-final CurrentUserData = FirebaseAuth.instance.currentUser;
--- lib/core/divider.dart
+++ lib/core/divider.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:project_spotifyclone/core/colors.dart';
 
