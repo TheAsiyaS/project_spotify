86a0f016fecf55edc61e47715ce49383a8c80833
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-11 20:02:46 +0530

    progress indicator special [package]

---
+++ lib/widgets/progressIndicator.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:project_spotifyclone/core/colors.dart';
+import 'package:project_spotifyclone/core/size.dart';
+import 'package:project_spotifyclone/widgets/texts.dart';
+
+
+class progress extends StatelessWidget {
+  const progress({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SpinKitFadingGrid(
+              size: 70,
+              color: darkgreen,
+            ),
+            h20,
+            text(
+              stringtext: 'Finding music for you...',
+              fontSize: 25,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
