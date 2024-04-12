c69294026d36a90652784f2e98f2489e1ee25102
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-12 11:40:24 +0530

    spotify token get function and essential ids

---
+++ lib/core/api/Spotify_token_acces.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:project_spotifyclone/core/api/api_and_ids.dart';
+
+Future<String> getSpotifyAccessToken() async {
+  // Encode the client ID and secret in Base64
+  String credentials = base64.encode(utf8.encode('$clientId:$clientSecret'));
+
+  // Spotify token endpoint
+  Uri tokenUrl = Uri.parse('https://accounts.spotify.com/api/token');
+
+  // Prepare the POST request body
+  var body = {
+    'grant_type': 'client_credentials',
+  };
+
+  // Send the POST request to get the access token
+  var response = await http.post(
+    tokenUrl,
+    headers: {
+      'Authorization': 'Basic $credentials',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: body,
+  );
+
+  if (response.statusCode == 200) {
+    var responseData = json.decode(response.body);
+    String accessToken = responseData['access_token'];
+    return accessToken;
+  } else {
+    throw Exception('Failed to obtain access token');
+  }
+}
---
+++ lib/core/api/api_and_ids.dart
@@ -0,0 +1,20 @@
+const String clientId = '9ba8296cb29d44f59c4c99590eb2208a';
+const String clientSecret = '2305c838e5a54d8b9fd4081389526968';
+const String redirectUri =
+    'https://project-spotify-64b99firebaseapp.com/__/auth/handler';
+
+const String TokenUrl = 'https://accounts.spotify.com/api/token';
+//https://accounts.spotify.com/authorize?response_type=code&client_id=9ba8296cb29d44f59c4c99590eb2208a&scope=user-read-playback-position&redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Fcallback
+
+const authcode =
+    'AQBSe9n48GBCV6N-bW6KsWuXNHytXOEADwzgTyPS8t5o4ggXHv7Aaaq4GHmWIrU2_NCp-Mfj-Pgd9OKtnRqrJFkvC2ltXEJ8lKqVhjXUZLuztLvv3EK1y9_EDmGPc8LwxjOJVl-_vaGaZqdBbgBnbUwPr-D8Rb9f3yNsckovMNBI3_WwxP1PHCwZB-ma-qC1vtvTVXUPgWKvJzWdjqnEFbpB3wVyVhCA5YkLlvPZEuEGRA_Pz9PePNOttHw9';
+/*
+https://project-spotify-64b99firebaseapp.com/__/auth/handler?
+code=AQBSe9n48GBCV6N-bW6KsWuXNHytXOEADwzgTyPS8t5o4ggXHv7Aaaq4GHmWIrU2_NCp-Mfj-Pgd9OKtnRqrJFkvC2ltXEJ8lKqVhjXUZLuztLvv3EK1y9_EDmGPc8LwxjOJVl-_vaGaZqdBbgBnbUwPr-D8Rb9f3yNsckovMNBI3_WwxP1PHCwZB-ma-qC1vtvTVXUPgWKvJzWdjqnEFbpB3wVyVhCA5YkLlvPZEuEGRA_Pz9PePNOttHw9
+ 
+ https://project-spotify-64b99firebaseapp.com/__/auth/handler?
+ code=AQAMH_-8JboXZjyrxrvZxshZOczFaivBN-AS6XrNdojm0LhqiFQcnHML5ko2FN6ndSfJnZq9poPiiWvrCxw9gbS9kerBTC1dHbLkbY5Tq6-zKnCsWChxp4QcZ74vNT54yjwPjnfvM8n1_c4R-ccplCyGXfnjZbxcwOZJoXEoURwTsdPPd72TmznpsaKwdrbiA6jr2Qa06jvISSnXEY_STA
+ 
+ https://project-spotify-64b99firebaseapp.com/__/auth/handler?
+ code=AQBbX3cfTX01rPviLommtze7GsYgzSn8_lcFbZA3jWyVP2A94yX0ujPvbut7CWv5FmuyubwtyKWtVbHhy72-gkddRvU-ipI6lhCPGQrAnffgbDnTkzbijc1AqpDpjpn765bqGRWGXs7kP0xAtJVVSSi-FTnhw5kFlG2kuLC9KU-y0V086hUwwxdfWpXV6ujJBjARGRZ1DAxbgrYBIeJpig
+ */
