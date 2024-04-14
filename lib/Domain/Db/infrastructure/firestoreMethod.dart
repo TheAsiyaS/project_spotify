fcf246531f9dde71c30c0bb59b71efd8a5b28c03
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-14 11:17:45 +0530

    function for Following and unfollowing artist

--- lib/Domain/Db/infrastructure/firestoreMethod.dart
+++ lib/Domain/Db/infrastructure/firestoreMethod.dart
@@ -7,8 +7,6 @@ class FirestoreMethod {
   //for add data to firebase
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  
-
   Future<bool> search_add(
       {required String profileImg,
       required String username,
@@ -38,6 +36,44 @@ class FirestoreMethod {
     try {
       if (uid != null) {
         await _firestore.collection('search').doc(uid).delete();
+        isok = true;
+      }
+      isok = true;
+    } catch (e) {
+      isok = false;
+    }
+    return isok;
+  }
+
+  Future<bool> follow_artists({
+    required artistid,
+    required profileImg,
+    required uid,
+    required username,
+  }) async {
+    bool isOk;
+    try {
+      final docUser = _firestore.collection('followartists').doc(artistid);
+      final data = ArtistModel(
+        artistid: artistid,
+        profileImg: profileImg,
+        uid: uid,
+        username: username,
+      );
+      final decodeJson = data.toJson();
+      await docUser.set(decodeJson);
+      isOk = true;
+    } catch (e) {
+      isOk = false;
+    }
+    return isOk;
+  }
+
+  Future<bool> unfollow_artists({required artistuid}) async {
+    bool isok;
+    try {
+      if (artistuid != null) {
+        await _firestore.collection('followartists').doc(artistuid).delete();
         isok = true;
       }
       isok = true;
