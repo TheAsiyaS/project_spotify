ad88040ab08b66e5f3fa96a9e7684f9e2288dc8a
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-14 19:51:07 +0530

    api calling : get -top tracks of artists

---
+++ lib/Infrastructure/TopTracks/toptrack_service.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:project_spotifyclone/Models/Playlists/playlist_model/track.dart';
+import 'package:project_spotifyclone/Models/topTracks/i_toptracks_repo.dart';
+import 'package:project_spotifyclone/core/api/Spotify_token_acces.dart';
+
+import '../../core/failures/mainFailure.dart';
+
+@LazySingleton(as: I_Toptrack_Repo)
+class ToptrackRepo implements I_Toptrack_Repo {
+  @override
+  Future<Either<mainFailure, List<Track>>> getToptracks(String id) async {
+    final String Access_token = await getSpotifyAccessToken();
+    try {
+      final Response response = await Dio(BaseOptions(
+        headers: {
+          'Authorization': 'Bearer $Access_token',
+        },
+      )).get('https://api.spotify.com/v1/artists/$id/top-tracks?country=KR');
+      // log(response.data.toString());
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        final Track_List = (response.data['tracks'] as List).map((e) {
+          return Track.fromJson(e);
+        }).toList();
+
+        return right(Track_List);
+      } else {
+        return left(const mainFailure.serverFailure());
+      }
+    } catch (e) {
+      return left(const mainFailure.clientFailure());
+    }
+  }
+}
