7ab9368386339e2885632f209a0789f20bc6a950
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-14 11:45:15 +0530

    service [i_repo] of playlist

---
+++ lib/Models/Playlists/I_playlist_repo.dart
@@ -0,0 +1,14 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:project_spotifyclone/Models/album/albummodel/item.dart';
+import 'package:project_spotifyclone/core/failures/mainFailure.dart';
+
+abstract class I_playlist_Repo {
+  Future<Either<mainFailure, List<Item>>> getPlaylistsItems(
+      List<String> playListIds);
+  Future<Either<mainFailure, List<Map<String, dynamic>>>> getplaylistoverview(
+      List<String> playListIds);
+
+  Future<Either<mainFailure, List<Map<String, dynamic>>>> getplaylistoverview1(
+      List<String> playListIds);
+}
