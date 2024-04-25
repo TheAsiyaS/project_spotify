208fec767246a8e2adfdbdc534a537fd6660216b
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-25 20:07:56 +0530

    main page bloc and injectable intialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_spotifyclone/Domain/DependencyInjuction/injection.dart';
+import 'package:project_spotifyclone/application/Album/album_bloc.dart';
+import 'package:project_spotifyclone/application/Artists/list_artists_bloc.dart';
+import 'package:project_spotifyclone/application/Search/search_bloc.dart';
+import 'package:project_spotifyclone/application/SongVlaues/songvalues_bloc.dart';
+import 'package:project_spotifyclone/application/playlistBloc/playlist_bloc.dart';
 
+import 'application/TopTrack/toptracklist_bloc.dart';
 import 'core/colors.dart';
 import 'widgets/NavigationBar.dart';
 
@@ -17,15 +25,25 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            scaffoldBackgroundColor: black,
-            brightness: Brightness.dark,
-            appBarTheme: const AppBarTheme(backgroundColor: black)),
-        home: navigationBar());
+    return MultiBlocProvider( 
+      providers: [
+        BlocProvider(create: (ctx) => getIt<ListArtistsBloc>()),
+        BlocProvider(create: (ctx) => getIt<PlaylistBloc>()),
+        BlocProvider(create: (ctx) => getIt<AlbumBloc>()),
+        BlocProvider(create: (ctx) => getIt<ToptracklistBloc>()),
+        BlocProvider(create: (ctx) => getIt<SearchBloc>()),
+        BlocProvider(create: (ctx) => SongvaluesBloc()),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              scaffoldBackgroundColor: black,
+              brightness: Brightness.dark,
+              appBarTheme: const AppBarTheme(backgroundColor: black)),
+          home: navigationBar()),
+    );
   }
 }
