eac2392f3d6d80cebae059700cdb1df024e4f9d1
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2024-04-27 19:42:02 +0530

    google user authentication [screen decide accoding to sign in]

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project_spotifyclone/Domain/DependencyInjuction/injection.dart';
+import 'package:project_spotifyclone/Domain/provider/signIn_working_class.dart';
 import 'package:project_spotifyclone/application/Album/album_bloc.dart';
 import 'package:project_spotifyclone/application/Artists/list_artists_bloc.dart';
 import 'package:project_spotifyclone/application/Search/search_bloc.dart';
 import 'package:project_spotifyclone/application/SongVlaues/songvalues_bloc.dart';
 import 'package:project_spotifyclone/application/playlistBloc/playlist_bloc.dart';
+import 'package:project_spotifyclone/presentation/SignUpbasedUi/signup_ui.dart';
+import 'package:project_spotifyclone/presentation/SignUpbasedUi/websignIn.dart';
+import 'package:project_spotifyclone/widgets/snapWaiting.dart';
+import 'package:provider/provider.dart';
 
 import 'application/TopTrack/toptracklist_bloc.dart';
 import 'core/colors.dart';
@@ -22,15 +29,14 @@ Future<void> main() async {
           messagingSenderId: '353992947181',
           projectId: 'project-spotify-ed3ad'));
   runApp(const MyApp());
-}    
-
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider( 
+    return MultiBlocProvider(
       providers: [
         BlocProvider(create: (ctx) => getIt<ListArtistsBloc>()),
         BlocProvider(create: (ctx) => getIt<PlaylistBloc>()),
@@ -45,7 +51,60 @@ class MyApp extends StatelessWidget {
               scaffoldBackgroundColor: black,
               brightness: Brightness.dark,
               appBarTheme: const AppBarTheme(backgroundColor: black)),
-          home: const navigationBar()),
+          home: SplashScreen()),
     );
   }
-}  
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Simulating a delay of 3 seconds using Future.delayed
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return ChangeNotifierProvider(
+          create: ((context) => GoogleSignInProvider()),
+          child: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              final provider = Provider.of<GoogleSignInProvider>(context);
+
+              if (provider.isSignedIn) {
+                return const Center(
+                  child: snapwaiting(color: spotify_green),
+                );
+              } else if (snapshot.hasData) {
+                return const navigationBar();
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: snapwaiting(color: spotify_green),
+                );
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: snapwaiting(color: red),
+                );
+              } else {
+                return kIsWeb ? const webSignIn() : const screen_signIn();
+              }
+            },
+          ),
+        );
+      }));
+    });
+
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                      'asset/image/some/images-removebg-preview.png'),
+                  fit: BoxFit.cover)),
+        ),
+      )),
+    );
+  }
+}
